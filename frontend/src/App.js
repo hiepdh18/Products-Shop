db3f6cd0175c1698c7eef9c2678ee04c127c8f83
Do Haong Hiep <hiep.dohoang.ncc@gmail.com>
2021-07-15 14:15:29 +0700

    delete axiosClient

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,54 @@
 import './App.css';
-import LoginForm from './components/LoginForm';
-import React, { useState } from 'react';
+// import LoginForm from './mainpages/Login';
+import React, {  useEffect } from 'react';
+import axios from 'axios'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom'
+
+
 
 function App() {
-  const adminUser = {
-    email: "admin@gmail.com",
-    password: "admin.123"
-  }
+  // const adminUser = {
+  //   email: "admin@gmail.com",
+  //   password: "admin.123"
+  // }
 
-  const [user, setUser] = useState({ name: "", email: "" })
-  const [error, setError] = useState("")
+  // const [user, setUser] = useState({ name: "", email: "" })
+  // const [error, setError] = useState("")
 
-  const Login = details => {
-    console.log(details)
-  }
+  // const Login = details => {
+  //   console.log(details)
+  // }
 
-  const Logout = () => {
-    console.log("Logout")
-    
-  }
+  // const Logout = () => {
+  //   console.log("Logout")
+
+  // }
+
+  useEffect(()=>{
+    const getType = async () => {
+      const types = await axios.get('/type')
+      console.log(types)
+    }
+    getType()
+  },[])
+
   return (
-    <div className="App">
-
-      <LoginForm></LoginForm>
-    </div>
+    <Router>
+      <Switch>
+        {/* <Route path="/" exact component={Products} />
+        <Route path="/detail/:id" exact component={DetailProduct} />
+        <Route path="/login" exact component={isLogged ? NotFound : Login} />
+        <Route path="/register" exact component={isLogged ? NotFound : Register} />
+        <Route path="/category" exact component={isAdmin ? Categories : NotFound} />
+        <Route path="/create_product" exact component={isAdmin ? CreateProduct : NotFound} />
+        <Route path="/edit_product/:id" exact component={isAdmin ? UpdateProduct : NotFound} />
+        <Route path="*" exact component={NotFound} /> */}
+      </Switch>
+    </Router>
   );
 }
 
